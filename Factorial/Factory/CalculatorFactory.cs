835b4bff88827b34f8aa22a635c07e6444bf9e4a
ZeroMajor <ZeroMajor@outlook.com>
2016-08-16 12:50:08 +0300

    Create instance of calculator only once, when requested at first time

--- Factorial/Factory/CalculatorFactory.cs
+++ Factorial/Factory/CalculatorFactory.cs
@@ -27,17 +27,35 @@ namespace Factorial.Factory
 	/// Provides method to create the required factorial calculator implementation
 	/// </summary>
 	public class CalculatorFactory
-	{		
+	{
+		private SimpleCalculator simpleCalculator;
+		private FastCalculator fastCalculator;
+		private BinarySplitCalculator binarySplitCalculator;
+
+		/// <summary>
+		/// Returns instance of Factorial Calculator of selected type
+		/// </summary>
+		/// <param name="calcType">Type of Factorial Calculator</param>
+		/// <returns>Instance of Factorial Calculator</returns>
 		public IFactorialCalculator GetFactorialCalculator(CalculatorType calcType)
 		{
 			switch(calcType)
 			{
 				case CalculatorType.Simple:
-					return new SimpleCalculator();
+					if (simpleCalculator == null)
+						simpleCalculator = new SimpleCalculator();
+					return simpleCalculator;
+
 				case CalculatorType.Fast:
-					return new FastCalculator();
+					if (fastCalculator == null)
+						fastCalculator = new FastCalculator();
+					return fastCalculator;
+
 				case CalculatorType.BinarySplit:
-					return new BinarySplitCalculator();
+					if (binarySplitCalculator == null)
+						binarySplitCalculator = new BinarySplitCalculator();
+					return binarySplitCalculator;
+
 				default:
 					throw new NotImplementedException("Such factorial calculation is not defined");
 			}
